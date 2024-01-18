9baafafea0cd87fe9f4b42dcb9798cfb3a92665d
ShroomaSapiens <syafiranurul49@gmail.com>
2024-01-18 15:28:50 +0700

    landing pej

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LandingPageController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [LoginController::class, 'index']);
+Route::get('/', [LandingPageController::class, 'index']);
